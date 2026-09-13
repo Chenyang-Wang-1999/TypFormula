45091ea9879bedb0db77993dcbd5d7a86dac02d7
Wang Chenyang <cy-wang21@mails.tsinghua.edu.cn>
2026-09-13 13:13:02 +0800

    为 release 构建脚本添加注释

--- build-release.cmd
+++ build-release.cmd
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 cd /d "%~dp0"
+@REM Set your python path by $env:TYPFORMULA_PYTHON = (Resolve-Path your/venv/path/python.exe).Path
 if defined TYPFORMULA_PYTHON (
   "%TYPFORMULA_PYTHON%" tools\build_release.py %*
 ) else (
